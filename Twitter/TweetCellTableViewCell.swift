bc7f7414527a6e082309f700106c1ddc2e54d318
jnuel <joynuel@gmail.com>
2020-10-11 16:47:08 -0500

    User can compose tweet, favorite and retweets tweets!

--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -10,9 +10,59 @@ import UIKit
 
 class TweetCellTableViewCell: UITableViewCell {
 
+    var favorited:Bool = false
+    var tweetId:Int = -1
+    
+    func setFavorite(_ isFavorited:Bool) {
+        favorited = isFavorited
+        if (favorited) {
+            favoriteButton.setImage(UIImage(named:"favor-icon-red"), for: UIControl.State.normal)
+        }
+        else {
+            favoriteButton.setImage(UIImage(named:"favor-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    func setRetweeted(_ isRetweeted:Bool) {
+        if (isRetweeted) {
+            print("retweeted")
+            retweetButton.setImage(UIImage(named:"retweet-icon-green"), for: UIControl.State.normal)
+        } else {
+            print("unretweeted")
+            retweetButton.setImage(UIImage(named:"retweet-icon"), for: UIControl.State.normal)
+        }
+    }
+    
+    
     @IBOutlet var profileImageView: UIImageView!
     @IBOutlet var usernameLabel: UILabel!
     @IBOutlet var tweetContent: UILabel!
+    @IBOutlet var retweetButton: UIButton!
+    @IBOutlet var favoriteButton: UIButton!
+    @IBAction func retweetTweet(_ sender: Any) {
+        TwitterAPICaller.client?.retweet(tweetId: tweetId, success: {
+            self.setRetweeted(true)
+        }, failure: { (error) in print("Error in retweeting: \(error)")})
+    }
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+        let tobeFavorited = !favorited
+        if (tobeFavorited) {
+            TwitterAPICaller.client?.favoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(true)
+            }, failure: { (error) in print("Favorite did not succeed: \(error)")
+            })
+        } else {
+            TwitterAPICaller.client?.unfavoriteTweet(tweetId: tweetId, success: {
+                self.setFavorite(false)
+            }, failure: { (error) in print("Unfavorite did not succeed: \(error)")
+            })
+        }
+    }
+    
+    
+    
+
     
     override func awakeFromNib() {
         super.awakeFromNib()
